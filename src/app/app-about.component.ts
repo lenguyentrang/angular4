fee5cc2b4252048f11cf95ae9c98826f422042ed
Le Nguyen Trang <lenguyentrang83@gmail.com>
2018-07-15 17:00:48 +0700

    create search page.

--- src/app/app-about.component.ts
+++ src/app/app-about.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   template: `
-  <div>
-    app-about works!
-  </div>
+      <weather *ngIf="id>0" [cityCode]=id></weather>
   `,
   styles: [``]
 })
 export class AppAboutComponent implements OnInit {
+    id = 0
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.route.params.subscribe( params => {
+        console.log(params)
+        this.id = params.id;
+    }
+  );
+}
 
   ngOnInit() {
-      this.http.get('http://weather.dev1/weather.php?command=location&woeid=2344116').subscribe(data => {
+      this.http.get('http://weather.dev1/weather.php?command=location&woeid='+this.id).subscribe(data => {
           console.log(data);
       });
   }
--- src/app/app-contact.component.ts
+++ src/app/app-contact.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-contact',
-  template: `
-  <div>
-    app-contact works!
-  </div>
-  `,
-  styles: [``]
+    template: `
+        <div *ngFor = "let city of results">
+        <weather  *ngIf="city.woeid>0" [cityCode]=city.woeid></weather>
+        </div>
+    `,
+    styles: [``]
 })
 export class AppContactComponent implements OnInit {
+    keyword = ''
+    results=[]
 
-  constructor() { }
+    constructor(private http: HttpClient, private route: ActivatedRoute) {
+        this.route.params.subscribe( params => {
+                console.log(params)
+                this.keyword = params.keyword;
+            }
+        );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.http.get('http://weather.dev1/weather.php?command=search&keyword='+this.keyword).subscribe(data => {
+            console.log(data);
+            this.results = data;
+
+
+        });
+    }
 
 }
